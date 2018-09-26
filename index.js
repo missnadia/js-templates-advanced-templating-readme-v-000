88f73d058a5c8884ba3d0f9d288544aa424af23b
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-26 01:19:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function loadIssues() {
+  var issues = {
+    state: "closed",
+    number: 5,
+    created_at: "2016-03-31 16:23:13 UTC",
+    body: "Instructions say GET /team and POST /newteam. Rspec wants GET /newteam and POST /team."
+  }
 
+  var template = Handlebars.compile(document.getElementbyId("issue-template").innerHTML);
+  var result = template(issues);
+  document.getElementsbyTagName("main")[0].innerHTML += result;
+  }
+}
